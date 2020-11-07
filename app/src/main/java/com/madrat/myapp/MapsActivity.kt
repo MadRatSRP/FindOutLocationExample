893a2509c2fdde14ea65cd829c04cbecbd263be3
MadRat96 <mischa.alpeew@yandex.ru>
2020-11-07 21:15:39 +0300

    app module: -Added Others(ViewBindingDelegate-Stable, EasyPermissions), Jetpack(AppCompat, ConstraintLayout), Android KTX(Fragment), Google(Maps, Location) dependencies -Added Project Language Target Versions description for Java and Kotlin

--- app/src/main/java/com/madrat/myapp/MapsActivity.kt
+++ app/src/main/java/com/madrat/myapp/MapsActivity.kt
@@ -1,7 +1,15 @@
 package com.madrat.myapp
 
 import androidx.appcompat.app.AppCompatActivity
+import com.google.android.gms.maps.SupportMapFragment
+import com.hoc081098.viewbindingdelegate.viewBinding
+import com.madrat.myapp.databinding.ActivityMapsBinding
 
-class MapsActivity : AppCompatActivity() {
-
+class MapsActivity: AppCompatActivity(R.layout.activity_maps) {
+    // ViewBinding
+    private val viewBinding by viewBinding<ActivityMapsBinding>()
+    
+    // SupportMapFragment
+    private lateinit var mapFragment: SupportMapFragment
+    
 }
